790d8e12b53a21ec7abe69f4600d4790e5abdd21
huycubi123 <huygiaphi123@gmail.com>
2025-03-27 00:53:27 +0700

    cho da

--- app/src/main/java/com/example/demosll/GiaoVienActivity.java
+++ app/src/main/java/com/example/demosll/GiaoVienActivity.java
@@ -5,45 +5,51 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class GiaoVienActivity extends AppCompatActivity {
-    Button btnQuanLyDiem, btnQuanLyHanhKiem, btnQuanLyHocPhi, btnLogout;
+    ListView lv;
+    TeacherAdapter adapter;
+    ArrayList<ListItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giao_vien);
 
-        // Ánh xạ View
-        btnQuanLyDiem = findViewById(R.id.btnQuanLyDiem);
-        btnQuanLyHanhKiem = findViewById(R.id.btnQuanLyHanhKiem);
-        btnQuanLyHocPhi = findViewById(R.id.btnQuanLyHocPhi);
-        btnLogout = findViewById(R.id.btnLogout);
+        lv = findViewById(R.id.lv);
 
-        // Sự kiện mở màn hình quản lý điểm số
-//        btnQuanLyDiem.setOnClickListener(v -> {
-//            startActivity(new Intent(GiaoVienActivity.this, QuanLyDiemActivity.class));
-//        });
-//
-//        // Sự kiện mở màn hình quản lý hạnh kiểm
-//        btnQuanLyHanhKiem.setOnClickListener(v -> {
-//            startActivity(new Intent(GiaoVienActivity.this, QuanLyHanhKiemActivity.class));
-//        });
-//
-//        // Sự kiện mở màn hình quản lý học phí
-//        btnQuanLyHocPhi.setOnClickListener(v -> {
-//            startActivity(new Intent(GiaoVienActivity.this, QuanLyHocPhiActivity.class));
-//        });
+        // Danh sách menu cho Giáo Viên
+        items = new ArrayList<>();
+        items.add(new ListItem(R.drawable.group2s, "Danh sách học sinh"));
+        items.add(new ListItem(R.drawable.group2m, "Cập nhật điểm"));
+        items.add(new ListItem(R.drawable.group2n, "Liên hệ phụ huynh"));
 
-        // Đăng xuất
-        btnLogout.setOnClickListener(v -> {
-            SharedPreferences sharedPreferences = getSharedPreferences("USER_SESSION", Context.MODE_PRIVATE);
-            sharedPreferences.edit().putBoolean("LOGGED_IN", false).apply();
-            startActivity(new Intent(GiaoVienActivity.this, LoginActivity.class));
-            finish();
+        adapter = new TeacherAdapter(this, items);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent myIntent;
+                if(i==0){
+                    myIntent=new Intent(GiaoVienActivity.this, DanhSachHocSinh.class);
+                    startActivity(myIntent);
+                }
+                if(i==1){
+                    myIntent=new Intent(GiaoVienActivity.this, CapNhatDiem.class);
+                    startActivity(myIntent);
+                }
+                if(i==2){
+                    myIntent=new Intent(GiaoVienActivity.this, LienHePhuHuynh.class);
+                    startActivity(myIntent);
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/example/demosll/HocSinhActivity.java
+++ app/src/main/java/com/example/demosll/HocSinhActivity.java
@@ -4,44 +4,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HocSinhActivity extends AppCompatActivity {
-    Button btnXemDiem, btnXemHanhKiem, btnXemHocPhi, btnLogout;
+import java.util.ArrayList;
 
+public class HocSinhActivity extends AppCompatActivity {
+    ListView lv;
+    StudentAdapter adapter;
+    ArrayList<ListItem> items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoc_sinh);
+        lv = findViewById(R.id.lv);
 
-        // Ánh xạ View
-        btnXemDiem = findViewById(R.id.btnXemDiem);
-        btnXemHanhKiem = findViewById(R.id.btnXemHanhKiem);
-        btnXemHocPhi = findViewById(R.id.btnXemHocPhi);
-        btnLogout = findViewById(R.id.btnLogout);
-
-//        // Sự kiện mở màn hình xem điểm số
-//        btnXemDiem.setOnClickListener(v -> {
-//            startActivity(new Intent(HocSinhActivity.this, XemDiemActivity.class));
-//        });
-
-        // Sự kiện mở màn hình xem hạnh kiểm
-//        btnXemHanhKiem.setOnClickListener(v -> {
-//            startActivity(new Intent(HocSinhActivity.this, XemHanhKiemActivity.class));
-//        });
-//
-//        // Sự kiện mở màn hình xem học phí
-//        btnXemHocPhi.setOnClickListener(v -> {
-//            startActivity(new Intent(HocSinhActivity.this, XemHocPhiActivity.class));
-//        });
-
-        // Đăng xuất
-        btnLogout.setOnClickListener(v -> {
-            SharedPreferences sharedPreferences = getSharedPreferences("USER_SESSION", Context.MODE_PRIVATE);
-            sharedPreferences.edit().putBoolean("LOGGED_IN", false).apply();
-            startActivity(new Intent(HocSinhActivity.this, LoginActivity.class));
-            finish();
+        // Danh sách menu cho Học Sinh
+        items = new ArrayList<>();
+        items.add(new ListItem(R.drawable.group20, "Thông tin của con"));
+        items.add(new ListItem(R.drawable.group2, "Kênh thông báo, tin nhắn "));
+        items.add(new ListItem(R.drawable.group21, "Thanh toán học phí"));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent myIntent;
+                if(i==0){
+                    myIntent=new Intent(HocSinhActivity.this,ThongTinHocSinh.class);
+                    startActivity(myIntent);
+                }
+                if(i==1){
+                    myIntent=new Intent(HocSinhActivity.this, KenhThongTin.class);
+                    startActivity(myIntent);
+                }
+                if(i==2){
+                    myIntent=new Intent(HocSinhActivity.this, ThanhToanHocPhi.class);
+                    startActivity(myIntent);
+                }
+            }
         });
+        adapter = new StudentAdapter(this, items);
+        lv.setAdapter(adapter);
     }
 }
